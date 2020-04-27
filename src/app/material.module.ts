40651e4f722f9670ce070ff1da30d877d3670e89
Gerardo Gonzalez <gerardo.gonzalez@naranja.com>
2020-04-27 00:39:17 -0300

    first versión of home page

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
-
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 
 @NgModule({
-    imports: [],
-    exports: []
+    imports: [MatButtonModule, MatIconModule, MatMenuModule],
+    exports: [MatButtonModule, MatIconModule, MatMenuModule]
 })
 export class MaterialModule {}
