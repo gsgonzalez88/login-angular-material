9125c31e457e700453c97c65b0722f83b625263f
Gerardo Gonzalez <gerardo.gonzalez@naranja.com>
2020-04-29 00:39:28 -0300

    wip: creating login inputs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,10 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 
-const routes: Routes = [{
-  path: '', component: HomeComponent
-},
-{ path: 'auth', component: LoginComponent }];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'auth', component: LoginComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatInputModule } from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @NgModule({
-    imports: [MatButtonModule, MatIconModule, MatMenuModule],
-    exports: [MatButtonModule, MatIconModule, MatMenuModule]
+    imports: [MatButtonModule, MatIconModule, MatMenuModule, MatInputModule, MatFormFieldModule],
+    exports: [MatButtonModule, MatIconModule, MatMenuModule, MatInputModule, MatFormFieldModule]
 })
 export class MaterialModule {}
