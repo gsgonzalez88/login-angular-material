8e2929d812f1455cb586f45987b00e1f828892ec
Gerardo Gonzalez <gerardo.gonzalez@naranja.com>
2020-05-02 00:36:51 -0300

    register page created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
+import { PrivateComponent } from './pages/private/private.component';
+import { SignupComponent } from './pages/signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    LoginComponent
+    LoginComponent,
+    PrivateComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
