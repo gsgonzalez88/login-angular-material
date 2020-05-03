1d9eb04a06bf42c6f832ab82fbf2ed14be152ac6
Gerardo Gonzalez <gerardo.gonzalez@naranja.com>
2020-05-03 15:06:25 -0300

    re-styling and eliminated duplicate code

--- src/app/services/authentication/auth.service.ts
+++ src/app/services/authentication/auth.service.ts
@@ -1,20 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { User } from 'src/app/models/user';
 import { Observable } from 'rxjs';
 
 @Injectable()
 export class AuthService {
 
-  user$: Observable<User>;
 
   constructor( private http: HttpClient) { 
 
   }
-
-  signUp(email: string, password:string){
-
-  }
-
 
 }
