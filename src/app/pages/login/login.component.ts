aa378c02efa92dd98f424d2aaea031c9f671daf9
Gerardo Gonzalez <gerardo.gonzalez@naranja.com>
2020-05-03 17:01:56 -0300

    adding more detail to login auth

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/authentication/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  invalidLogin: boolean = false;
+
+  constructor(private router: Router, private authService: AuthService) { }
 
   ngOnInit() {
+  }
+
+  signIn(credentials) {
+    this.authService.login(credentials)
+      .subscribe(result => {
+        if (result)
+          this.router.navigate(['/']);
+        else 
+          this.invalidLogin = true;
+      })
   }
 
 }
