89015993c06fdee31f11654e3cf60b803edd3584
Gerardo Gonzalez <gerardo.gonzalez@naranja.com>
2020-05-06 01:26:30 -0300

    first approach of backend connection for testing

--- src/app/services/authentication/auth.service.ts
+++ src/app/services/authentication/auth.service.ts
@@ -1,25 +1,24 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { map } from 'rxjs/operators';
 
 @Injectable()
-export class AuthService {
-
-
-  constructor( private http: HttpClient) { 
+export class AuthService {  
+  constructor( private http: HttpClient, private router: Router) { 
 
   }
 
-  login(credentials) {
-    return this.http.post('/api/authenticate', JSON.stringify(credentials));
-      
+  login(credentials) { 
+    this.http.post('http://localhost:3000/api/authenticate', credentials)
+    .subscribe(data => {
+      if(data){
+        this.router.navigate(['/private']);
+      } else {
+        this.router.navigate(['/'])
+      }      
+    })
   }
-
-  logout() {
-
-  }
-
-  isLoggedIn(){
-    return false;
-  }
+  
 
 }
