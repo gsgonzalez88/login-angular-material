6fbc3301ef860bfd41153f8a31ee189e0eff32cc
Gerardo Gonzalez <gerardo.gonzalez@naranja.com>
2020-08-05 23:59:48 -0300

    responsive register page

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,25 +1,24 @@
-import { Component, OnInit } from '@angular/core';
-import { AuthService } from 'src/app/services/authentication/auth.service';
-import { NgForm } from '@angular/forms';
+import { Component, OnInit } from "@angular/core";
+import { AuthService } from "src/app/services/authentication/auth.service";
+import { NgForm, FormGroup, FormControl, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  selector: "app-register",
+  templateUrl: "./register.component.html",
+  styleUrls: ["./register.component.scss"],
 })
 export class RegisterComponent implements OnInit {
+  
+  hide = true;
+  
+  constructor(private authService: AuthService) {}
 
-  constructor(private authService: AuthService) { }
-
-  ngOnInit() {
-  }
-  signUp(form: NgForm){
-    
-    if(form.invalid){
-      console.log('invalid')
+  ngOnInit() {}
+  signUp(form: NgForm) {
+    if (form.invalid) {
+      console.log("invalid");
       return;
     }
     this.authService.createUser(form.value.email, form.value.password);
   }
-
 }
